ae351e40b4f772a978f85e3caca06b513ed542c8
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-05-29 15:50:12 +0100

    added files for junit udemy course, written 3 tests to check boolean conditions

---
+++ HealthyCoderApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+d
+4src/main/java/com/healthycoderapp/BMICalculator.java,c\a\cabc9957dfe02f2d8e627eda390f2119d722301c
+h
+8src/test/java/com/healthycoderapp/BMICalculatorTest.java,2\3\2331b41af6cbccfa5739030706f38db905c58b90
---
+++ HealthyCoderApp/src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,62 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+
+    @Test
+    void should_ReturnTrue_When_DietRecommended(){
+
+        // given
+        double weight = 89.0;
+        double height = 1.72;
+
+        //when
+        boolean recommended = BMICalculator.isDietRecommended(weight,height);
+
+        //then
+        assertTrue(recommended);
+
+
+        //instead of:
+//        assertTrue(BMICalculator.isDietRecommended(89.0,1.72));
+    }
+    @Test
+    void should_ReturnFalse_When_DietNotRecommended(){
+
+        // given
+        double weight = 50.0;
+        double height = 1.92;
+
+        //when
+        boolean recommended = BMICalculator.isDietRecommended(weight,height);
+
+        //then
+        assertFalse(recommended);
+
+
+        //instead of:
+//        assertFalse(BMICalculator.isDietRecommended(50.0,1.92));
+    }
+
+    @Test
+    void should_ThrowArithmeticException_When_HeightZero(){
+
+        // given
+        double weight = 0;
+        double height = 0;
+
+        //when
+        Executable executable = () -> BMICalculator.isDietRecommended(weight,height);
+            // we need to do this when testing for exceptions on 00 values because the exception
+            // will be thrown before the assertion.
+            // here we create an executable and pass it to the assertion so it can be executed.
+        //then
+        assertThrows(ArithmeticException.class, executable);
+        
+    }
+}
